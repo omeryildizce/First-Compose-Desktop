be7f33a39ad960f3947d435fc3ad2eb40c190e82
omeryildizce <omeryildizce@gmail.com>
2023-02-14 19:55:19 +0300

    Create build.gradle.kts

---
+++ StopWatchComposeDesktop/build.gradle.kts
@@ -0,0 +1,54 @@
+import org.jetbrains.compose.compose
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("multiplatform")
+    id("org.jetbrains.compose")
+
+
+}
+subprojects{
+    dependencies{
+        val implementation by configurations
+        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:0.30.1")
+    }
+}
+group = "com.omeryildizce"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    google()
+    mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+
+}
+
+
+kotlin {
+    jvm {
+        compilations.all {
+            kotlinOptions.jvmTarget = "11"
+        }
+        withJava()
+    }
+    sourceSets {
+        val jvmMain by getting {
+            dependencies {
+                implementation(compose.desktop.currentOs)
+            }
+        }
+        val jvmTest by getting
+    }
+}
+
+compose.desktop {
+    application {
+        mainClass = "MainKt"
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "StopWatchComposeDesktop"
+            packageVersion = "1.0.0"
+        }
+    }
+}
